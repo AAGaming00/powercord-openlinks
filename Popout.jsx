ebf7ce3b1517ffb9623ecaa49227cb2ad0b5edd8
AAGaming00 <aa@bigdumb.gq>
2020-11-14 22:21:34 -0500

    temp fix

--- Popout.jsx
+++ Popout.jsx
@@ -1,17 +1,32 @@
 const { React, getModule } = require('powercord/webpack');
-const { Spinner } = require('powercord/components');
+const { PopoutWindow, Spinner } = require('powercord/components');
+const Titlebar = require('./Titlebar');
 function Popout (props) {
-  const divRef = div => {
-    props.resolve(div.ownerDocument.defaultView);
-    const win = div.ownerDocument.defaultView;
-    delete win.opener;
-    delete win.require;
-    delete win.DiscordNative;
-    win.location.href = props.url;
-  };
-
+  const [ loading, setLoading ] = React.useState(true);
+  /*
+   *   const divRef = div => {
+   *     console.log(div.ownerDocument.defaultView);
+   *     props.resolve(div.ownerDocument.defaultView);
+   *     const win = div.ownerDocument.defaultView;
+   *     delete win.opener;
+   *     delete win.require;
+   *     delete win.DiscordNative;
+   *     // win.location.href = props.url;
+   *   };
+   */
   return (
-    <div ref = {divRef} className = 'popout popoutspinner'><Spinner/></div>
+    <div className = 'popout'>
+      <Titlebar type='WINDOWS' windowKey={props.windowId} themeOverride={props.theme} />
+      <iframe src = {props.url}
+        onLoad = {() => setLoading(false)}
+        className = 'popoutcontentFrame'
+        allowtransparency="true"
+        sandbox='allow-forms allow-modals allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-top-navigation'
+      />
+      <div className = {`popoutspinner${loading ? '' : ' popoutspinnerbehind'}`}>
+        <Spinner/>
+      </div>
+    </div>
   );
 }
 
@@ -19,9 +34,12 @@ function Popout (props) {
 module.exports = function (props, name, id) {
   return new Promise((resolve) => {
     const popoutModule = getModule([ 'setAlwaysOnTop', 'open' ], false);
-    popoutModule.open(id, () =>
-      React.createElement(Popout, { ...props,
-        resolve })
+    popoutModule.open(id, (key) =>
+      React.createElement(PopoutWindow, {
+        windowKey: key,
+        title: name
+      }, React.createElement(Popout, { ...props,
+        resolve }))
     );
   });
 };
---
+++ dumb.jsx
@@ -0,0 +1,27 @@
+const { React, getModule } = require('powercord/webpack');
+const { Spinner } = require('powercord/components');
+function Popout (props) {
+  const divRef = div => {
+    props.resolve(div.ownerDocument.defaultView);
+    const win = div.ownerDocument.defaultView;
+    delete win.opener;
+    delete win.require;
+    delete win.DiscordNative;
+    win.location.href = props.url;
+  };
+
+  return (
+    <div ref = {divRef} className = 'popout popoutspinner'><Spinner/></div>
+  );
+}
+
+
+module.exports = function (props, name, id) {
+  return new Promise((resolve) => {
+    const popoutModule = getModule([ 'setAlwaysOnTop', 'open' ], false);
+    popoutModule.open(id, () =>
+      React.createElement(Popout, { ...props,
+        resolve })
+    );
+  });
+};
